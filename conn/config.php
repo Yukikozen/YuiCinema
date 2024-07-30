aa25e6f1d9cfe7a31122b3822071021cffff8616
Yui <yuizen084@gmail.com>
2024-07-30 21:58:24 +0800

    Add files via upload

---
+++ conn/config.php
@@ -0,0 +1,21 @@
+<?php
+//include_once header.php';
+define("DBHOST","localhost");
+define("DBNAME","cinema");
+define("DBUSER","root");
+define("DBPASS","");
+
+//smtp
+define("SMTPUSER", "yuizen084@gmail.com");
+define("SMTPPASS", "oxkmrhduxrvyrrtt");
+
+//define("SMTPPASS", "hfhubpzlhwdqujpi");
+define("SMTPSEC", "ssl");
+define("SMTPHOST", "smtp.gmail.com");
+//define("SMTPPORT", "465");
+define("SMTPPORT", "465");
+
+define("SMTPFROM", "yuizen084@gmail.com");
+define("SMTPFROMNAME", "YuiCinema");
+define("SMTPSSL", "ssl://smtp.gmail.com");
+?>
