e560147e95d7ccfabd7c4cc8c3c55ac37b04f0d6
Yui <yuizen084@gmail.com>
2025-05-18 22:22:04 +0800

    update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,87 +1,85 @@
 <?php
 session_start();
-$_SESSION["admin_username"]=null;
-$error="";
+$_SESSION["admin_username"] = null;
+$error = "";
 
-if(isset($_POST["btn_login"]))
-{
-   $email_id=$_POST["log_email"];
-   $paswrd_log=$_POST["log_psw"];
+// Database connection
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "yuicinema"; // <-- change to your DB name
 
-    if("yuizen08@gmail.com"==$email_id)
-    {
-        if("zzz1"==$paswrd_log)
-        {
-            $_SESSION["admin_username"]=$email_id;
-            header("Location:dashboard.php");
-        }
-        else
-        {
-            $error="Invalid Password";
-        }
-           
-    }
-    else
-    {
-        $error="Invalid Email";
-    } 
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+if (isset($_POST["btn_login"])) {
+    $email_id = $_POST["log_email"];
+    $paswrd_log = $_POST["log_psw"];
+
+    $sql = "SELECT password FROM admin WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($paswrd_log, $row['password'])) {
+            $_SESSION["admin_username"] = $email_id;
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            $error = "Invalid Password";
+        }
+    } else {
+        $error = "Invalid Email";
+    }
+}
+
+$conn->close();
 ?>
-
-
-
-
 
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <title>Admin Panel</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  
-        <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          crossorigin="anonymous">
 
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
-  <body>
-      
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6" style="margin:auto;">
+            <form method="post">
+                <div class="container" style="color:maroon;">
+                    <center>
+                        <h1> Admin Login</h1>
+                    </center>
+                    <hr>
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" style="border-radius:30px;" placeholder="Enter Email" name="log_email" id="email" required>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6" style="margin:auto;">
-                <form method="post">
-                    <div class="container" style="color:maroon;">
-                        <center>
-                            <h1> Admin Login</h1>
-                        </center>
-                     <hr>
-                        <label for="email"><b>Email</b></label>
-                        <input type="text" style="border-radius:30px;" placeholder="Enter Email" name="log_email" id="email" required>
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" style="border-radius:30px;" placeholder="Enter Password" name="log_psw" id="psw" required>
 
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" style="border-radius:30px;" placeholder="Enter Password" name="log_psw" id="psw" required>
-
-                        <button type="submit" name="btn_login" class="btn" style="background-color:maroon;color:white;">Login</button>
-                    </div>
-                </form>
-                <p style="color:maroon;margin-left:1%;"><?php echo $error; ?></p>
-            </div>
+                    <button type="submit" name="btn_login" class="btn" style="background-color:maroon;color:white;">Login</button>
+                </div>
+            </form>
+            <p style="color:maroon;margin-left:1%;"><?php echo $error; ?></p>
         </div>
     </div>
+</div>
 
-
-      
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<!--    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
-  
-  <?php    include_once 'admin_footer.php';?>
-  </body>
-  
+<?php include_once 'admin_footer.php'; ?>
+</body>
 </html>
